36f25d1122f9948735b2a34d7b7ab27af09a023a
Zumrud03 <nusretovazumrud@gmail.com>
2024-03-10 23:57:28 +0400

    navbar fixed&footer added

--- src/layouts/Navbar/Navbar.jsx
+++ src/layouts/Navbar/Navbar.jsx
@@ -1,10 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Navbar.scss";
 
 function Navbar() {
   const [selectedOption, setSelectedOption] = useState("");
   const [isOpen, setIsOpen] = useState(false);
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+  const [stickyNav, setStickyNav] = useState(false);
+  // const [stickySidebar, setStickySidebar] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", stickNavbar);
+
+    return () => {
+      window.removeEventListener("scroll", stickNavbar);
+    };
+  }, [isSideBarOpen, stickyNav]);
+
+  const stickNavbar = () => {
+    if (typeof window !== "undefined") {
+      let windowHeight = window.scrollY;
+      setStickyNav(windowHeight > 100);
+      setIsSideBarOpen(false);
+
+      // Güncellenmiş kısım
+      setStickyNav((prevStickyNav) => {
+        document.querySelector('.sidebar').style.top = prevStickyNav ? '0' : '45.5px';
+        return prevStickyNav;
+      });
+    }
+  };
+
+  // useEffect(() => {
+  //   window.addEventListener("scroll", stickySidebar);
+
+  //   return () => {
+  //     window.removeEventListener("scroll", stickySidebar);
+  //   };
+  // }, []);
+  // const stickSideBar = () => {
+  //   if (window !== undefined) {
+  //     let windowHeight = window.scrollY;
+  //     windowHeight > 100 ? setStickySidebar(true) : setStickySidebar(false);
+  //   }
+  // };
 
   const options = ["Azərbaycan", "English", "Русский"];
 
@@ -17,136 +56,224 @@ function Navbar() {
     setIsOpen(!isOpen);
   };
   return (
-    <div className="navbar_container">
-      <div className="navbar_upside_bg">
-        <div className="navbar_upside">
-          <img
-            className="navbar_upside_img"
-            src="https://upload.wikimedia.org/wikipedia/commons/4/4c/Heydar_Aliyev_%28crop%29.jpg"
-            alt=""
-          />
-          <p className="navbar_upside_text">
-            Azərbyacan Respublikası Vəkillər Kollegiyası Akademiyası
-          </p>
-          <img
-            className="navbar_upside_flag"
-            src="https://upload.wikimedia.org/wikipedia/commons/d/dd/Flag_of_Azerbaijan.svg"
-            alt=""
-          />
+    <div className="navbar">
+      <div className="navbar_container">
+        <div className="navbar_upside_bg">
+          <div className="navbar_upside">
+            <img
+              className="navbar_upside_img"
+              src="https://upload.wikimedia.org/wikipedia/commons/4/4c/Heydar_Aliyev_%28crop%29.jpg"
+              alt=""
+            />
+            <p className="navbar_upside_text">
+              Azərbyacan Respublikası Vəkillər Kollegiyası Akademiyası
+            </p>
+            <img
+              className="navbar_upside_flag"
+              src="https://upload.wikimedia.org/wikipedia/commons/d/dd/Flag_of_Azerbaijan.svg"
+              alt=""
+            />
+          </div>
         </div>
-      </div>
-      <div className="navbar">
-        <div className="navbar_logo">
-          <img
-            src="https://smartdemowp.com/city-govt/wp-content/themes/citygovt/assets/images/logo.png"
-            alt=""
-          />
-        </div>
-        <ul className="navbar_navmenu">
-          <li>
-            <NavLink
-              to="/"
-              style={({ isActive }) => {
-                return {
-                  color: isActive ? "#0176ff" : "",
-                };
-              }}
-            >
-              Əsas səhifə
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/qanunvericilik"
-              style={({ isActive }) => {
-                return {
-                  color: isActive ? "#0176ff" : "",
-                };
-              }}
-            >
-              Qanunvericilik
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/xeberler"
-              style={({ isActive }) => {
-                return {
-                  color: isActive ? "#0176ff" : "",
-                };
-              }}
-            >
-              Xəbərlər
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/jurnallar"
-              style={({ isActive }) => {
-                return {
-                  color: isActive ? "#0176ff" : "",
-                };
-              }}
-            >
-              Jurnallar
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/elektronKitabxana"
-              style={({ isActive }) => {
-                return {
-                  color: isActive ? "#0176ff" : "",
-                };
-              }}
-            >
-              Elektron kitabxana
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/heyet"
-              style={({ isActive }) => {
-                return {
-                  color: isActive ? "#0176ff" : "",
-                };
-              }}
-            >
-              Heyət
-            </NavLink>
-          </li>
-        </ul>
-        <div className="navbar_login_lang">
-          <div className="navbar_lang">
-            <div className={`select-header`} onClick={toggleDropdown}>
-              <div>
-                {selectedOption || "Azərbaycan"}
+        <div className={`navbarBot  ${stickyNav ? "fixed" : ""}`}>
+          <div className="navbarBot_bottom">
+            <div className="navbarBot_logo">
+              <img src="../src/assets/image/lawyer-logo.png" alt="" />
+            </div>
+            <ul className="navbarBot_navmenu">
+              <li>
+                <NavLink
+                  to="/"
+                  style={({ isActive }) => {
+                    return {
+                      color: isActive ? "#0176ff" : "",
+                    };
+                  }}
+                >
+                  Əsas səhifə
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/qanunvericilik"
+                  style={({ isActive }) => {
+                    return {
+                      color: isActive ? "#0176ff" : "",
+                    };
+                  }}
+                >
+                  Qanunvericilik
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/xeberler"
+                  style={({ isActive }) => {
+                    return {
+                      color: isActive ? "#0176ff" : "",
+                    };
+                  }}
+                >
+                  Xəbərlər
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/jurnallar"
+                  style={({ isActive }) => {
+                    return {
+                      color: isActive ? "#0176ff" : "",
+                    };
+                  }}
+                >
+                  Jurnallar
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/elektronKitabxana"
+                  style={({ isActive }) => {
+                    return {
+                      color: isActive ? "#0176ff" : "",
+                    };
+                  }}
+                >
+                  Elektron kitabxana
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/heyet"
+                  style={({ isActive }) => {
+                    return {
+                      color: isActive ? "#0176ff" : "",
+                    };
+                  }}
+                >
+                  Heyət
+                </NavLink>
+              </li>
+            </ul>
+            <div className="navbarBot_login_lang">
+              <div className="navbarBot_lang">
+                <div className={`select-header`} onClick={toggleDropdown}>
+                  <div>{selectedOption || "Azərbaycan"}</div>
+                  <i
+                    className={`${
+                      isOpen ? "fa-solid fa-angle-up" : "fa-solid fa-angle-down"
+                    }`}
+                  ></i>
+                </div>
+                {isOpen && (
+                  <ul className="navbarBot_select">
+                    {options.map((option, index) => (
+                      <li
+                        className="navbarBot_option"
+                        key={index}
+                        onClick={() => handleSelectChange(option)}
+                      >
+                        {option}
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+              <div className="navbarBot_login">
+                <i className="fa-solid fa-user"></i>
+                <p>Şəxsi kabinet</p>
               </div>
               <i
-                className={`${
-                  isOpen ? "fa-solid fa-angle-up" : "fa-solid fa-angle-down"
-                }`}
+                className="fa-solid fa-bars"
+                onClick={() => setIsSideBarOpen(!isSideBarOpen)}
               ></i>
             </div>
-            {isOpen && (
-              <ul className="navbar_select">
-                {options.map((option, index) => (
-                  <li
-                    className="navbar_option"
-                    key={index}
-                    onClick={() => handleSelectChange(option)}
-                  >
-                    {option}
-                  </li>
-                ))}
-              </ul>
-            )}
           </div>
-          <div className="navbar_login">
+        </div>
+        <div className={`sidebar ${isSideBarOpen ? "" : "isSideBarOpen"} `}>
+          <div
+            className="sidebar_exit"
+            onClick={() => setIsSideBarOpen(!isSideBarOpen)}
+          >
+            <i className="fa-solid fa-xmark"></i>
+          </div>
+          <ul className="sidebar_nav">
+            <li>
+              <NavLink
+                to="/"
+                style={({ isActive }) => {
+                  return {
+                    color: isActive ? "#0176ff" : "",
+                  };
+                }}
+              >
+                Əsas səhifə
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/qanunvericilik"
+                style={({ isActive }) => {
+                  return {
+                    color: isActive ? "#0176ff" : "",
+                  };
+                }}
+              >
+                Qanunvericilik
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/xeberler"
+                style={({ isActive }) => {
+                  return {
+                    color: isActive ? "#0176ff" : "",
+                  };
+                }}
+              >
+                Xəbərlər
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/jurnallar"
+                style={({ isActive }) => {
+                  return {
+                    color: isActive ? "#0176ff" : "",
+                  };
+                }}
+              >
+                Jurnallar
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/elektronKitabxana"
+                style={({ isActive }) => {
+                  return {
+                    color: isActive ? "#0176ff" : "",
+                  };
+                }}
+              >
+                Elektron kitabxana
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/heyet"
+                style={({ isActive }) => {
+                  return {
+                    color: isActive ? "#0176ff" : "",
+                  };
+                }}
+              >
+                Heyət
+              </NavLink>
+            </li>
+          </ul>
+          <div className="sidebar_login">
             <i className="fa-solid fa-user"></i>
             <p>Şəxsi kabinet</p>
           </div>
-          <i className="fa-solid fa-bars"></i>
         </div>
       </div>
     </div>
