30eaba83d59eb6463990dfa22456a3f07f3e1981
Samir Orucov <samiroruclu10@gmail.com>
2024-03-10 04:35:00 +0400

    contact us section created and responsive okay

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AboutSliderSection from "../components/AboutSliderSection/AboutSliderSection";
+import ContactUsComponent from "../components/ContactUsComponent/ContactUsComponent";
 
 function Home() {
   return (
     <div>
       <AboutSliderSection />
+      <ContactUsComponent/>
     </div>
   );
 }
