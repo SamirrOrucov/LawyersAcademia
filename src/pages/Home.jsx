16f22c5e0da6813ba7b71d9ade3d4f4dfaf92c80
Samir Orucov <samiroruclu10@gmail.com>
2024-03-11 18:24:43 +0400

    fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,17 @@
-
 import React from "react";
 import AboutSliderSection from "../components/AboutSliderSection/AboutSliderSection";
 import ContactUsComponent from "../components/ContactUsComponent/ContactUsComponent";
-
-import React from 'react'
-import "./Home.scss"
-import VideosSection from '../components/HomeComponents/VideosSection/VideosSection'
+import "./Home.scss";
+import VideosSection from "../components/HomeComponents/VideosSection/VideosSection";
 
 function Home() {
   return (
     <div>
-
-   
-  
-
       <AboutSliderSection />
-    <VideosSection/>
-
-      <ContactUsComponent/>
+      <VideosSection />
+      <ContactUsComponent />
     </div>
   );
-
-   
-   
-  
 }
 
 export default Home;
