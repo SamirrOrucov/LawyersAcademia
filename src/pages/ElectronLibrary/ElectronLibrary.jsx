c3e99be1da91cd3ded54e0f6487210f5fe3dfa30
Samir Orucov <samiroruclu10@gmail.com>
2024-03-28 01:00:21 +0400

    electron library detail and search, category added

--- src/pages/ElectronLibrary/ElectronLibrary.jsx
+++ src/pages/ElectronLibrary/ElectronLibrary.jsx
@@ -1,39 +1,63 @@
 import React, { useEffect, useState } from "react";
 import "./ElectronLibrary.scss";
+import { Link } from "react-router-dom";
+import Search from "antd/es/input/Search";
+import { Select } from "antd";
+
 function ElectronLibrary() {
   const [books, setBooks] = useState([]);
-  const data = [
-    { img: "src/assets/image/header.png", title: "Əmək məcəlləsi" },
-    { img: "src/assets/image/header.png", title: "Əmək məcəlləsi" },
-    { img: "src/assets/image/header.png", title: "Əmək məcəlləsi" },
-    { img: "src/assets/image/header.png", title: "Əmək məcəlləsi" },
-    { img: "src/assets/image/header.png", title: "Əmək məcəlləsi" },
-    { img: "src/assets/image/header.png", title: "Əmək məcəlləsi" },
-    { img: "src/assets/image/header.png", title: "Əmək məcəlləsi" },
-    { img: "src/assets/image/header.png", title: "Əmək məcəlləsi" },
-    { img: "src/assets/image/header.png", title: "Əmək məcəlləsi" },
-    { img: "src/assets/image/header.png", title: "Əmək məcəlləsi" },
-    { img: "src/assets/image/header.png", title: "Əmək məcəlləsi" },
-    { img: "src/assets/image/header.png", title: "Əmək məcəlləsi" },
-    { img: "src/assets/image/header.png", title: "Əmək məcəlləsi" },
-  ];
+  const [filteredBooks, setFilteredBooks] = useState([]);
+  const [noResults, setNoResults] = useState(false);
+  const [isSearching, setIsSearching] = useState(false);
+  const options = [];
+  for (let i = 10; i < 36; i++) {
+    options.push({
+      value: i.toString(36) + i,
+      label: i.toString(36) + i,
+    });
+  }
+  const handleChange = (value) => {
+    console.log(`selected ${value}`);
+  };
   async function getBooks() {
     const response = await fetch("http://localhost:3003/book");
     const data = await response.json();
+    setIsSearching(true);
     setBooks(data);
+    setTimeout(() => {
+      setIsSearching(false);
+    }, 1000);
   }
+
   useEffect(() => {
-    
-  getBooks()
-    
-  }, [])
-  
+    getBooks();
+  }, []);
+
+  const onSearch = (value, e, info) => {
+    setIsSearching(true);
+    const filteredBooksArray = books.filter((book) =>
+      book.title.toLowerCase().includes(value.toLowerCase())
+    );
+    if (filteredBooksArray.length === 0) {
+      setTimeout(() => {
+        setNoResults(true);
+      }, 1000);
+    } else {
+      setNoResults(false);
+      setFilteredBooks(filteredBooksArray);
+    }
+    setTimeout(() => {
+      setIsSearching(false);
+    }, 1000);
+  };
   return (
     <div className="library">
       <div className="library_container">
         <div className="library_container_top">
           <div className="library_container_top_header">
-            <p>Elektron Kitabxana</p>
+            <div className="library_container_top_text">
+              <p>Elektron Kitabxana</p>
+            </div>
             <div className="lines">
               <div className="line One"></div>
               <div className="line Two"></div>
@@ -43,21 +67,62 @@ function ElectronLibrary() {
             </div>
           </div>
           <div className="library_container_top_search">
-            <input type="search" name="" id="" />
+            <Select
+              mode="tags"
+              style={{
+                width: "200px",
+              }}
+              placeholder="Kateqoriyalar"
+              onChange={handleChange}
+              options={options}
+            />
+            <Search
+              placeholder="input search text"
+              allowClear
+              onSearch={onSearch}
+              style={{
+                width: "240px",
+              }}
+              enterButton
+            />
+            {noResults && <p>Sorry this book does not exist</p>}
           </div>
         </div>
-        <div className="library_container_main">
-          {data.map((item) => (
-            <div className="library_container_main_card">
-              <div className="image">
-                <img src={item.img} alt="" />
-              </div>
-              <div className="bookTitle">
-                <p>{item.title}</p>
-              </div>
-            </div>
-          ))}
-        </div>
+        {isSearching ? (
+          <div className="loader_container">
+            <div className="loader"></div>
+          </div>
+        ) : (
+          <div className="library_container_main">
+            {filteredBooks.length > 0
+              ? filteredBooks.map((item) => (
+                  <div key={item._id} className="library_container_main_card">
+                    <div className="image">
+                      <img src={item.bookPhoto} alt="" />
+                      <Link to={"/library/" + item._id}>
+                        <button className="hiddenButton">Keçid et</button>
+                      </Link>
+                    </div>
+                    <div className="bookTitle">
+                      <p>{item.title}</p>
+                    </div>
+                  </div>
+                ))
+              : books.map((item) => (
+                  <div key={item._id} className="library_container_main_card">
+                    <div className="image">
+                      <img src={item.bookPhoto} alt="" />
+                      <Link to={"/library/" + item._id}>
+                        <button className="hiddenButton">Keçid et</button>
+                      </Link>
+                    </div>
+                    <div className="bookTitle">
+                      <p>{item.title}</p>
+                    </div>
+                  </div>
+                ))}
+          </div>
+        )}
       </div>
     </div>
   );
--- src/pages/ElectronLibraryDetail/ElectronLibraryDetail.jsx
+++ src/pages/ElectronLibraryDetail/ElectronLibraryDetail.jsx
@@ -1,36 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ElectronLibraryDetail.scss";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 function ElectronLibraryDetail() {
   const { id } = useParams();
+  const [book, setBook] = useState([]);
+  async function getBooks() {
+    const response = await fetch("http://localhost:3003/book/" + id);
+    const data = await response.json();
+    // setIsSearching(true);
+    setBook(data);
+    // setTimeout(() => {
+    //   setIsSearching(false);
+    // }, 1000);
+  }
+
+  useEffect(() => {
+    getBooks();
+  }, []);
+  console.log(book);
   return (
     <div className="libraryDetail">
       <div className="libraryDetail_container">
         <div className="libraryDetail_container_top">
-          {/* { */}
           <p>
             <Link to={"/library"}>Elektron Kitabxana</Link> /
-            <span> Kitab adı</span>
+            <span> {book.title}</span>
           </p>
-          {/* } */}
         </div>
         <div className="libraryDetail_container_main">
-          <div className="libraryDetail_container_container_main_left">
-            <img src="" alt="" />
+          <div className="libraryDetail_container_main_left">
+            <img className="bookFace" src={book.bookPhoto} alt="" />
           </div>
-          <div className="libraryDetail_container_container_main_right">
-            <p className="title">title</p>
-            <p className="detail">detail</p>
+          <div className="libraryDetail_container_main_right">
+            <p className="title">{book.title}</p>
+            <p className="detail">{book.description}</p>
             <p className="author">
-              {" "}
-              <span className="bold">Müəllif</span> Əmir Əliyev
+              <span className="bold">Müəllif :</span> {book.author}
             </p>
             <p className="date">
               {" "}
-              <span className="bold">Nəşr tarixi</span> 2013
+              <span className="bold">Nəşr tarixi :</span> 2013
             </p>
             <div className="button">
-              <button>Kitabı yüklə</button>
+              <a href={book.bookFile} download={"bookFile"}>
+                {" "}
+                <button class="button-30" role="button">Kitabı yüklə</button>
+
+              </a>
             </div>
           </div>
         </div>
